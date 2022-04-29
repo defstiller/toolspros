26a17c5a09704c08f9d924857d58ff1d0c511f51
defstiller <81063003+defstiller@users.noreply.github.com>
2022-04-29 02:06:55 -0500

    added nav tag for navbar

--- src/components/header/navBar/NavBar.jsx
+++ src/components/header/navBar/NavBar.jsx
@@ -4,23 +4,25 @@ import classes from "./navBar.module.css";
 
 function NavBar() {
 	return(
-		<ul className={classes.NavBar}>
-			<li>
-				Featured
-			</li>
+		<nav>
+			<ul className={classes.NavBar}>
+				<li>
+					Featured
+				</li>
 
-			<li>
-				About
-			</li>
+				<li>
+					About
+				</li>
 
-			<li>
-				Reviews
-			</li>
-			
-			<li>
-				Contact
-			</li>
-		</ul>
+				<li>
+					Reviews
+				</li>
+				
+				<li>
+					Contact
+				</li>
+			</ul>
+		</nav>
 	);
 }
 
