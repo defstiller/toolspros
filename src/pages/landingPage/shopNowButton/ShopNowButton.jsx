1833b9adb2d7ec43592fb309e78db0bc9c3fa18f
defstiller <81063003+defstiller@users.noreply.github.com>
2022-04-30 04:00:33 -0500

    added shop not and css for it

---
+++ src/pages/landingPage/shopNowButton/ShopNowButton.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import classes from "./shopNowButton.module.css";
+
+function ShopNowButton() {
+
+	return (
+		<div className={classes.buttonDiv}>
+			<button>
+				Shop Now
+			</button>
+		</div>
+	);
+
+}
+
+export default ShopNowButton;
