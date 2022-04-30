ca47d4c218dee0ac5a4d56488337e57c87d39adc
defstiller <81063003+defstiller@users.noreply.github.com>
2022-04-30 05:41:19 -0500

    added shop page layout

---
+++ src/layouts/shopPage/shopPageLayout.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Header from "../../components/header/Header";
+import BackgroundVideo from "../../pages/landingPage/backgroundVideo/BackgroundVideo";
+
+function ShopPageLayout() {
+
+	return(
+		<>
+			<Header />
+			<BackgroundVideo />
+		</>
+	);
+}
+
+export default ShopPageLayout;
