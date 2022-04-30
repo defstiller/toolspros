67fefbfbefc479f4ef92d7fc6d56d99ff9388c75
defstiller <81063003+defstiller@users.noreply.github.com>
2022-04-30 04:00:07 -0500

    added tagline ( text on landing page ) and css for it

---
+++ src/pages/landingPage/tagline/Tagline.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import classes from "./tagline.module.css";
+
+function Tagline() {
+
+	return (
+		<div className={classes.Tagline}>
+			<h1>
+				Tools for Pros by Pros
+			</h1>
+			<p>
+				Nam libero tempore, com soluta nobis est eligendi optio comque nihil impedit quo minus id quod maxime 
+				placeat facere possium, omnis voluptas assumenda est, omnis dolor
+				Nam libero tempore, com soluta nobis est eligendi optio comque nihil impedit quo minus id quod maxime 
+				placeat facere possium, omnis voluptas assumenda est, omnis dolor
+			</p>
+		</div>
+	);
+}
+
+export default Tagline;
