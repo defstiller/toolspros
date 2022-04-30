fce1b2f559be0853c42be6915f8066853839bf0a
defstiller <81063003+defstiller@users.noreply.github.com>
2022-04-30 05:11:53 -0500

    fixed missing semi

--- src/layouts/landingPage/LandingPageLayout.jsx
+++ src/layouts/landingPage/LandingPageLayout.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import Header from "../../components/header/Header";
 import BackgroundVideo from "../../components/backgroundVideo/BackgroundVideo";
 import Tagline from "../../pages/landingPage/tagline/Tagline";
-import ShopNowButton from "../../pages/landingPage/shopNowButton/ShopNowButton"
+import ShopNowButton from "../../pages/landingPage/shopNowButton/ShopNowButton";
 
 
 function LandingPageLayout() {
