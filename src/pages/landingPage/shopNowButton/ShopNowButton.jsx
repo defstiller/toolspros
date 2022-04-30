4a04df68b3b830396aa133b5c079f2742f73e4cc
defstiller <81063003+defstiller@users.noreply.github.com>
2022-04-30 05:42:07 -0500

    added navigation on click -- home > shop

--- src/pages/landingPage/shopNowButton/ShopNowButton.jsx
+++ src/pages/landingPage/shopNowButton/ShopNowButton.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import {useNavigate} from "react-router-dom";
 
 import classes from "./shopNowButton.module.css";
 
 function ShopNowButton() {
+	
+	const navigate = useNavigate();
+
+	function handleShopNowClick() {
+		const newPath = "./shop";
+		navigate(newPath);
+	}
 
 	return (
 		<div className={classes.buttonDiv}>
-			<button>
+			<button onClick={() => handleShopNowClick()}>
 				Shop Now
 			</button>
 		</div>
