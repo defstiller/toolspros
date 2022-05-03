c9d40857c557592900b9b8a73093a924f5782f6e
defstiller <81063003+defstiller@users.noreply.github.com>
2022-05-03 04:23:03 -0500

    updated get product to return erorr and loading, will return productcardsmall, this is just a first version of faster setup

--- src/pages/adminPage/getProduct/GetProduct.jsx
+++ src/pages/adminPage/getProduct/GetProduct.jsx
@@ -1,15 +1,21 @@
-import React from "react";
+import React, {useEffect} from "react";
 
 import useAddGetData from "../../../logic/firebaseLogic/firebaseDB/useAddGetData";
 
-function GetProduct() {
-	const {loading, data, error} = useAddGetData("products");
+import ProductCardSmall from "../../../components/productCard/productCardSmall/ProductCardSmall";
 
+function GetProduct() {
+	const {loading, error, receivedData, getData} = useAddGetData();
+
+	useEffect(() => {
+		getData("products");
+	}, []);
 
 	return(
 		<div>
-			{loading || data.map(product => {
-				return <p key={product.description}>{product.description}</p>;
+			{error && <p>error</p>}
+			{loading || receivedData.map(product => {
+				return <ProductCardSmall product={product} key={product.name}/>;
 			})}
 			{loading && <p>Loading...</p>}
 		</div>
