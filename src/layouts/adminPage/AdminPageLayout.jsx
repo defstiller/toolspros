0fbd9c10f146b775cb8a7f90525427321430d774
defstiller <81063003+defstiller@users.noreply.github.com>
2022-05-02 23:34:44 -0500

    created admin page with add product option

---
+++ src/layouts/adminPage/AdminPageLayout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import GetProduct from "../../pages/adminPage/getProduct/GetProduct";
+import AddProduct from "../../pages/adminPage/addProduct/AddProduct";
+function AdminPageLayout() {
+
+	return (<>
+		<AddProduct />
+	</>
+	);
+}
+
+export default AdminPageLayout;
