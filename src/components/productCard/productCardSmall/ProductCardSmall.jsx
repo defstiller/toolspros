5aa351a823b05d5912c72e9af67b93c613948d52
defstiller <81063003+defstiller@users.noreply.github.com>
2022-05-03 04:23:27 -0500

    changed to show all data about product to see what am i working with

--- src/components/productCard/productCardSmall/ProductCardSmall.jsx
+++ src/components/productCard/productCardSmall/ProductCardSmall.jsx
@@ -2,20 +2,29 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function ProductCardSmall(props) {
-	const {products} = props;
+	const {product} = props;
 	return(
-		<main>
-			{products.map(product =>{
-				return (
-					<article key={product.name}>
-						{product.name}
-					</article>);
-			})}
-		</main>
+		<article>
+			<p>
+				{product.name}
+			</p>
+			<p>
+				{product.description}
+			</p>
+			<p>{product.isFeatured}</p>
+			<p>{product.price}</p>
+			<p>{product.shipping}</p>
+			<p>{product.style}</p>
+			<img src={product.imgUrl} />
+			<p>{product.category}</p>
+
+		</article>
+
 	);
 }
 ProductCardSmall.propTypes = {
-	products: PropTypes.array
+	product: PropTypes.any,
+	imgUrl: PropTypes.any
 };
 
 export default ProductCardSmall;
