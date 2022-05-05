e75e8ceaa113f31a342d5fadfc84a7a3b5d21cf3
defstiller <81063003+defstiller@users.noreply.github.com>
2022-05-05 10:30:22 -0500

    added product info page layout

---
+++ src/layouts/productInfoPage/ProductInfoPageLayout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import ProductInfo from "../../pages/productPage/productInfo/ProductInfo";
+
+function ProductInfoPageLayout() {
+
+	return (
+		<ProductInfo />
+	);
+}
+
+export default ProductInfoPageLayout;
