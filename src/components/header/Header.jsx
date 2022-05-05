5f9a86f0770a42d2017bffedd6915b63244829f4
defstiller <81063003+defstiller@users.noreply.github.com>
2022-05-05 10:24:33 -0500

    added link to home page on logo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,19 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 import LogoSvg from "../../assets/svgsReactReady/logoSvg/LogoSvg";
 import CartButtonSvg from "./cartButtonSvg/CartButtonSvg";
 import NavBar from "./navBar/NavBar";
 
-import classes from "./header.module.css"
+import classes from "./header.module.css";
 
 function HeaderLayout() {
 
 	return (
 		<header className={classes.header}>
-			<LogoSvg />
+			<Link to="/" >
+				<LogoSvg />
+			</Link>
 			<NavBar />
 			<CartButtonSvg />
 		</header>
