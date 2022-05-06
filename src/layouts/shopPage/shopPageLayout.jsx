0a0fe8d3393e0b6879599378339e679e867e0917
defstiller <81063003+defstiller@users.noreply.github.com>
2022-05-06 17:58:14 -0500

    changed to show featuredandlist instead of separate files

--- src/layouts/shopPage/shopPageLayout.jsx
+++ src/layouts/shopPage/shopPageLayout.jsx
@@ -2,17 +2,14 @@ import React from "react";
 
 import Header from "../../components/header/Header";
 import SearchBar from "../../pages/shopPage/searchBar/SearchBar";
-import FeaturedProducts from "../../pages/shopPage/featuredProducts/FeaturedProducts";
-import ProductList from "../../pages/shopPage/productList/ProductList";
-
+import FeaturedAndList from "../../pages/shopPage/FeaturedAndList";
 function ShopPageLayout() {
 
 	return(
 		<>
 			<Header />
 			<SearchBar />
-			<FeaturedProducts />
-			<ProductList />
+			<FeaturedAndList />
 		</>
 	);
 }
