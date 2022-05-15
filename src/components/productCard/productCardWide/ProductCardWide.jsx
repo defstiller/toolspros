e4b1afaac710fed4fcb979f35a974e840c50d95f
defstiller <81063003+defstiller@users.noreply.github.com>
2022-05-15 14:16:14 -0500

    first version of wide product card

--- src/components/productCard/productCardWide/ProductCardWide.jsx
+++ src/components/productCard/productCardWide/ProductCardWide.jsx
@@ -1,22 +1,55 @@
 import React from "react";
+import { useNavigate, Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import { useCart } from "react-use-cart";
 
-import handleQuantityChange from "../../../logic/products/handleQuantityChange";
+import classes from "./productCardWide.module.css";
 
 function ProductCardWide(props) {
-	const {products} = props;
-	return (
-		<div>
-			{products.map(product => {
-				return <article key={product.name}>{product.name}</article>;
-			})}
-			<nav>All Featured Products</nav>
-		</div>
+	const {product, handleQuantityChange, handleProductClick, removeItem} = props;
+	const { addItem, inCart, getItem } = useCart();
+
+	const productInCart = getItem(product.id);
+	return(
+		<article className={classes.cardArticle}>
+			<img src={product.imgUrl}  onClick={() => handleProductClick(product)} className={classes.productImg}/>
+			<div>
+				<p>{product.name}</p>
+				<p>{product.description}</p>
+				<p>Price: ${product.price}</p>
+			</div>
+
+			{product.shipping > 0 
+			&& <p>Shipping: {product.shipping}</p>}
+
+			{
+				inCart(product.id) ?<>
+					<div className={classes.changeAmountBtns}>
+						<div>
+							<button onClick={() => handleQuantityChange(false, productInCart)}>-</button>
+							<p>{productInCart.quantity}</p>
+							<button onClick={() => handleQuantityChange(true, productInCart)}>+</button>
+							<button onClick={() => removeItem(product.id)}>&times;</button>
+						</div>
+					</div>
+				</>
+					: 
+					<button onClick={() => addItem(product, 1)}>Add to cart</button>
+			}
+					
+
+		</article>
+
 	);
 }
-
 ProductCardWide.propTypes = {
-	products: PropTypes.array
+	product: PropTypes.any,
+	imgUrl: PropTypes.string,
+	cartArray: PropTypes.any,
+	setCartArray: PropTypes.any,
+	handleQuantityChange: PropTypes.any,
+	handleProductClick: PropTypes.any,
+	removeItem: PropTypes.any
 };
 
 export default ProductCardWide;
