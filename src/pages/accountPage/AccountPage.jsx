2c4d8b072e0515a176bae15165d369404515facd
defstiller <81063003+defstiller@users.noreply.github.com>
2022-05-21 11:35:52 -0500

    added account page

---
+++ src/pages/accountPage/AccountPage.jsx
@@ -0,0 +1,14 @@
+import React, {useContext} from "react";
+import { updateProfile } from "firebase/auth";
+
+import {AuthContext} from "../../context/context";
+
+function AccountPage() {
+	const {isUser} = useContext(AuthContext); 
+	console.log(isUser);
+	return(
+		<h1>Hello {isUser.displayName || "friend"}!</h1>
+	);
+}
+
+export default AccountPage;
