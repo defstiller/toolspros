dab56e3068f28b173959061d197258646ea796f4
defstiller <81063003+defstiller@users.noreply.github.com>
2022-05-21 12:51:39 -0500

    added dropdrown menu with mouse over and click out, renamed classes css to styles, will move logic later

--- src/components/header/HeaderLayout.jsx
+++ src/components/header/HeaderLayout.jsx
@@ -1,25 +1,67 @@
-import React from "react";
+import React, {useContext, useState, useEffect, useRef} from "react";
 import {Link} from "react-router-dom";
 
 import LogoSvg from "../../assets/svgsReactReady/logoSvg/LogoSvg";
-import CartButtonSvg from "./cartButtonSvg/CartButtonSvg";
+import CartButtonSvg from "../../assets/svgsReactReady/cartButtonSvg/CartButtonSvg";
+import HeaderMobile from "./headerMobile/NavBarMobile";
+import AccountSvg from "../../assets/svgsReactReady/account/AccountSvg";
 import NavBar from "./navBar/NavBar";
 
-import classes from "./header.module.css";
+import {ScreenResizeContext} from "../../context/context";
+
+import styles from "./header.module.css";
 
 function HeaderLayout() {
+	const {width} = useContext(ScreenResizeContext);
+	const [dropDown, setDropdown] = useState(false);
+	const dropDownDiv = useRef()
+	useEffect(() => {
+		if(dropDown ) {
+			document.addEventListener("mouseup", function(event) {
+				const div = dropDownDiv.current;
+				if(div && !div.contains(event.target)){
+					setDropdown(false);
+				} 
 
-	return (
-		<header className={classes.header}>
+				
+			});
+		}
+		return;
+	}, [dropDown]);
+	
+	return <div className={styles.headerDiv}>
+		<header className={styles.header}>
 			<Link to="/" >
 				<LogoSvg />
 			</Link>
-			<NavBar />
+			<NavBar styles={styles}/>
+			{/* { width > 768 
+				?
+				<NavBar styles={styles}/>
+				:
+				<HeaderMobile />
+			} */}
 			<Link to="/cart">
-				<CartButtonSvg />
+				<CartButtonSvg styles={styles}/>
 			</Link>
+			<AccountSvg 
+				styles={styles} 
+				onMouseEnter={() => setDropdown(true)}
+			/>
 		</header>
-	);
+		{dropDown && (
+			<div 
+				className={styles.dropDown} 
+				ref={dropDownDiv} 
+				style={{display: dropDown}}
+				onMouseEnter={() => setDropdown(true)}
+			>
+				<p>Account</p>
+				<p>Sign Out</p>
+			</div>
+		)}
+	</div>;
+
 }
 
 export default HeaderLayout;
