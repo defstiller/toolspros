2564078d00e9aaed7015cef48de23155f722f639
defstiller <81063003+defstiller@users.noreply.github.com>
2022-05-22 21:53:31 -0500

    added close dropdown on click outside of div

--- src/components/header/HeaderLayout.jsx
+++ src/components/header/HeaderLayout.jsx
@@ -1,32 +1,32 @@
 import React, {useContext, useState, useEffect, useRef} from "react";
 import {Link} from "react-router-dom";
+import { getAuth, signOut } from "firebase/auth";
 
 import LogoSvg from "../../assets/svgsReactReady/logoSvg/LogoSvg";
 import CartButtonSvg from "../../assets/svgsReactReady/cartButtonSvg/CartButtonSvg";
 import HeaderMobile from "./headerMobile/NavBarMobile";
 import AccountSvg from "../../assets/svgsReactReady/account/AccountSvg";
 import NavBar from "./navBar/NavBar";
+import DropDown from "./dropdown/DropDown";
 
-import {ScreenResizeContext} from "../../context/context";
+import {ScreenResizeContext, AuthContext} from "../../context/context";
 
 import styles from "./header.module.css";
 
 function HeaderLayout() {
 	const {width} = useContext(ScreenResizeContext);
 	const [dropDown, setDropdown] = useState(false);
-	const dropDownDiv = useRef()
+	const dropDownDiv = useRef();
 	useEffect(() => {
-		if(dropDown ) {
-			document.addEventListener("mouseup", function(event) {
-				const div = dropDownDiv.current;
+		if(dropDown) {
+			const div = dropDownDiv.current;
+			document.addEventListener("mouseup", function hideDropDown(event) {
 				if(div && !div.contains(event.target)){
 					setDropdown(false);
 				} 
-
-				
+				return document.removeEventListener("mouseup", hideDropDown);			
 			});
 		}
-		return;
 	}, [dropDown]);
 	
 	return <div className={styles.headerDiv}>
@@ -46,18 +46,15 @@ function HeaderLayout() {
 			</Link>
 			<AccountSvg 
 				styles={styles} 
-				onMouseEnter={() => setDropdown(true)}
+				onClick={() => setDropdown(true)}
 			/>
 		</header>
 		{dropDown && (
 			<div 
-				className={styles.dropDown} 
+				className={styles.dropDown}  
 				ref={dropDownDiv} 
-				style={{display: dropDown}}
-				onMouseEnter={() => setDropdown(true)}
 			>
-				<p>Account</p>
-				<p>Sign Out</p>
+				<DropDown />
 			</div>
 		)}
 	</div>;
