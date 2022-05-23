85e915274d0e3017cf3d8f9b39278b9964baeb9e
defstiller <81063003+defstiller@users.noreply.github.com>
2022-05-22 21:30:09 -0500

    created dropdown for account in navbar

---
+++ src/components/header/dropdown/DropDown.jsx
@@ -0,0 +1,41 @@
+import React, { useRef, useContext} from "react";
+import {Link} from "react-router-dom";
+import PropTypes from "prop-types";
+import { getAuth, signOut } from "firebase/auth";
+
+import { AuthContext } from "../../../context/context";
+
+function DropDown() {
+	const {auth, isUser} = useContext(AuthContext);
+	function handleSignOut() {
+		signOut(auth).then(() => {
+			// Sign-out successful.
+			console.log("signed out")
+		}).catch((error) => {
+			// An error happened.
+			console.log(error)
+		});
+	}
+	return (
+		<>
+			<Link to="/account">
+				<button>Account</button>
+			</Link>
+			{isUser 
+				? <button onClick={() =>  handleSignOut()}
+				>Sign Out</button>
+				:
+				<Link to="/auth">
+					<button>Sign in</button>
+				</Link>
+			}
+		</>
+	);
+}
+DropDown.propTypes = {
+	styles: PropTypes.any,
+	dropDown: PropTypes.any,
+	isUser: PropTypes.any
+};
+
+export default DropDown;
