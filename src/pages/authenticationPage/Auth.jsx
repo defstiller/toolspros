167c1c649037a5d82fc530d7bd9709c88408b413
defstiller <81063003+defstiller@users.noreply.github.com>
2022-05-22 21:30:41 -0500

    added sign in with email and password

--- src/pages/authenticationPage/Auth.jsx
+++ src/pages/authenticationPage/Auth.jsx
@@ -1,7 +1,8 @@
 import React, {useState, useRef} from "react";
-import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword} from "firebase/auth";
 
 function Auth() {
+	const [isRegister, setIsRegister] = useState(true);
 	const [authInfo, setAuthInfo] = useState({
 		email: "",
 		password: ""
@@ -18,22 +19,37 @@ function Auth() {
 
 	const auth = getAuth();
 	function handleAuthWithEmail(event, authInfo) {
-		event.preventDefault()
+		event.preventDefault();
 		const {email, password} = authInfo;
-		createUserWithEmailAndPassword(auth, email, password)
-			.then((userCredential) => {
-			// Signed in 
-				const user = userCredential.user;
-				console.log(user)
-			// ...
-			})
-			.catch((error) => {
-				const errorCode = error.code;
-				const errorMessage = error.message;
-			// ..
-			});
+		if(isRegister) {
+			createUserWithEmailAndPassword(auth, email, password)
+				.then((userCredential) => {
+				// Signed in 
+					const user = userCredential.user;
+					console.log(user)
+				// ...
+				})
+				.catch((error) => {
+					const errorCode = error.code;
+					const errorMessage = error.message;
+				// ..
+				});
+
+		} else {
+			signInWithEmailAndPassword(auth, email, password)
+				.then((userCredential) => {
+				// Signed in 
+					const user = userCredential.user;
+				// ...
+				})
+				.catch((error) => {
+					const errorCode = error.code;
+					const errorMessage = error.message;
+				});
+		}
 
 	}
+
 	onAuthStateChanged(auth, (user) => {
 		if (user) {
 		// User is signed in, see docs for a list of available properties
@@ -49,11 +65,30 @@ function Auth() {
 	});
 	return (
 		<div>
+			{isRegister ?
+				<p>Sign up</p>
+				:
+				<p>Log in</p>
+			}
 			<form onSubmit={() => handleAuthWithEmail(window.event, authInfo)}>
-				<input placeholder="email" name="email" value={authInfo.email} onChange={handleChange}/>
-				<input placeholder="password" name="password" value={authInfo.password} onChange={handleChange}/>
+				<input 
+					placeholder="email" 
+					name="email" 
+					value={authInfo.email} 
+					onChange={handleChange}/>
+				<input 
+					placeholder="password" 
+					name="password" 
+					type="password"
+					value={authInfo.password} 
+					onChange={handleChange}/>
 				<button type="submit">Submit</button>
 			</form>
+			{isRegister ?
+				<button onClick={() => setIsRegister(false)}>Already registered?</button>
+				:
+				<button onClick={() => setIsRegister(true)}>Register Instead</button>
+			}
 		</div>
 	)
 
