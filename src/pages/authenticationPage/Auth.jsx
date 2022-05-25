16c0889574af23795a02ac890c48d57ba42c401b
defstiller <81063003+defstiller@users.noreply.github.com>
2022-05-24 20:14:04 -0500

    changed html tags, added modal on auth change and errors, added loading indicator

--- src/pages/authenticationPage/Auth.jsx
+++ src/pages/authenticationPage/Auth.jsx
@@ -1,7 +1,22 @@
-import React, {useState, useRef} from "react";
-import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword} from "firebase/auth";
+import React, {useState, useEffect} from "react";
+import { 
+	getAuth, 
+	createUserWithEmailAndPassword, 
+	onAuthStateChanged, 
+	signInWithEmailAndPassword
+} from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+
+import Modal from "../../components/modal/Modal";
+import LoadingModal from "../../components/modal/loadingModal/LoadingModal";
+
+import styles from "./auth.module.css";
 
 function Auth() {
+	const auth = getAuth();
+	const navigate = useNavigate();
+	const [isModal, setIsModal] = useState({});
+	const [isLoading, setIsLoading] = useState(false);
 	const [isRegister, setIsRegister] = useState(true);
 	const [authInfo, setAuthInfo] = useState({
 		email: "",
@@ -16,80 +31,113 @@ function Auth() {
 			[name]: value
 		});
 	}
-
-	const auth = getAuth();
-	function handleAuthWithEmail(event, authInfo) {
-		event.preventDefault();
+	function handleSubmitClick(event) { 
 		const {email, password} = authInfo;
+		event.preventDefault();
+		setIsLoading(true);
 		if(isRegister) {
 			createUserWithEmailAndPassword(auth, email, password)
 				.then((userCredential) => {
-				// Signed in 
-					const user = userCredential.user;
-					console.log(user)
-				// ...
+					// Signed in 
+					setIsModal({
+						content: 
+								<h1>Succesfully created account, reddirecting to shop page.</h1>, 
+						isOpen: true, 
+						delay: 5000});
+					// ...
 				})
 				.catch((error) => {
 					const errorCode = error.code;
 					const errorMessage = error.message;
-				// ..
-				});
-
-		} else {
+					setIsModal({
+						content: <>
+							<h1>An error occured</h1>
+							<p>{errorCode}</p>
+							<p>{errorMessage}</p>
+							<button onClick={() => setIsModal({})}>Close</button>
+						</>,
+						isOpen: true, 
+						delay: 50000});
+					// ..
+				})
+				.finally(() => setIsLoading(false));
+		}
+						
+		else {
 			signInWithEmailAndPassword(auth, email, password)
 				.then((userCredential) => {
-				// Signed in 
+					// Signed in 
 					const user = userCredential.user;
-				// ...
+					setIsModal({
+						content: 
+								<h1>Succesfully logged in as {user}, reddirecting to shop page.</h1>, 
+						isOpen: true, 
+						delay: 5000});
+					// ...
 				})
 				.catch((error) => {
 					const errorCode = error.code;
 					const errorMessage = error.message;
-				});
+					setIsModal({
+						content: <>
+							<h1>An error occured</h1>
+							<p>{errorCode}</p>
+							<p>{errorMessage}</p>
+							<button onClick={() => setIsModal({})}>Close</button>
+						</>,
+						isOpen: true, 
+						delay: 50000});
+				})
+				.finally(() => setIsLoading(false));
 		}
-
 	}
-
+	
 	onAuthStateChanged(auth, (user) => {
 		if (user) {
 		// User is signed in, see docs for a list of available properties
 		// https://firebase.google.com/docs/reference/js/firebase.User
-			const uid = user.uid;
-			console.log(uid)
+			// const uid = user.uid;
+			navigate("/shop");
 		// ...
-		} else {
-		// User is signed out
-		// ...
-		console.log("signed out")
 		}
 	});
 	return (
-		<div>
+		<main>
+			<LoadingModal styles={styles} isLoading={isLoading}/>
+			{isModal.isOpen && <Modal 
+				isModal={isModal} 
+				setIsModal={setIsModal}
+				styles={styles}
+			/>}
 			{isRegister ?
 				<p>Sign up</p>
 				:
 				<p>Log in</p>
 			}
-			<form onSubmit={() => handleAuthWithEmail(window.event, authInfo)}>
+			<form onSubmit={(event) => {
+				handleSubmitClick(event)
+			}}>
 				<input 
 					placeholder="email" 
 					name="email" 
 					value={authInfo.email} 
+					autoComplete="username"
 					onChange={handleChange}/>
 				<input 
 					placeholder="password" 
 					name="password" 
 					type="password"
 					value={authInfo.password} 
+					autoComplete="current-password"
 					onChange={handleChange}/>
-				<button type="submit">Submit</button>
+				<button type="submit" disabled={isLoading}>Submit</button>
 			</form>
 			{isRegister ?
 				<button onClick={() => setIsRegister(false)}>Already registered?</button>
 				:
 				<button onClick={() => setIsRegister(true)}>Register Instead</button>
 			}
-		</div>
+		</main>
 	)
 
 }
