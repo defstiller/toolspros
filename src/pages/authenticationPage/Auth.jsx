2e3ae71e71f3a618ab224406d26030a0b379884f
defstiller <81063003+defstiller@users.noreply.github.com>
2022-05-24 23:07:02 -0500

    moved button click logic to separate file, added div for css and class names

--- src/pages/authenticationPage/Auth.jsx
+++ src/pages/authenticationPage/Auth.jsx
@@ -10,6 +10,7 @@ import { useNavigate } from "react-router-dom";
 import Modal from "../../components/modal/Modal";
 import LoadingModal from "../../components/modal/loadingModal/LoadingModal";
 
+import handleAuthSubmitClick from "./handleAuthSubmitClick";
 import styles from "./auth.module.css";
 
 function Auth() {
@@ -31,66 +32,6 @@ function Auth() {
 			[name]: value
 		});
 	}
-	function handleSubmitClick(event) { 
-		const {email, password} = authInfo;
-		event.preventDefault();
-		setIsLoading(true);
-		if(isRegister) {
-			createUserWithEmailAndPassword(auth, email, password)
-				.then((userCredential) => {
-					// Signed in 
-					setIsModal({
-						content: 
-								<h1>Succesfully created account, reddirecting to shop page.</h1>, 
-						isOpen: true, 
-						delay: 5000});
-					// ...
-				})
-				.catch((error) => {
-					const errorCode = error.code;
-					const errorMessage = error.message;
-					setIsModal({
-						content: <>
-							<h1>An error occured</h1>
-							<p>{errorCode}</p>
-							<p>{errorMessage}</p>
-							<button onClick={() => setIsModal({})}>Close</button>
-						</>,
-						isOpen: true, 
-						delay: 50000});
-					// ..
-				})
-				.finally(() => setIsLoading(false));
-		}
-						
-		else {
-			signInWithEmailAndPassword(auth, email, password)
-				.then((userCredential) => {
-					// Signed in 
-					const user = userCredential.user;
-					setIsModal({
-						content: 
-								<h1>Succesfully logged in as {user}, reddirecting to shop page.</h1>, 
-						isOpen: true, 
-						delay: 5000});
-					// ...
-				})
-				.catch((error) => {
-					const errorCode = error.code;
-					const errorMessage = error.message;
-					setIsModal({
-						content: <>
-							<h1>An error occured</h1>
-							<p>{errorCode}</p>
-							<p>{errorMessage}</p>
-							<button onClick={() => setIsModal({})}>Close</button>
-						</>,
-						isOpen: true, 
-						delay: 50000});
-				})
-				.finally(() => setIsLoading(false));
-		}
-	}
 	
 	onAuthStateChanged(auth, (user) => {
 		if (user) {
@@ -102,41 +43,45 @@ function Auth() {
 		}
 	});
 	return (
-		<main>
+		<main className={styles.main}>
 			<LoadingModal styles={styles} isLoading={isLoading}/>
 			{isModal.isOpen && <Modal 
 				isModal={isModal} 
 				setIsModal={setIsModal}
 				styles={styles}
 			/>}
-			{isRegister ?
-				<p>Sign up</p>
-				:
-				<p>Log in</p>
-			}
-			<form onSubmit={(event) => {
-				handleSubmitClick(event)
-			}}>
-				<input 
-					placeholder="email" 
-					name="email" 
-					value={authInfo.email} 
-					autoComplete="username"
-					onChange={handleChange}/>
-				<input 
-					placeholder="password" 
-					name="password" 
-					type="password"
-					value={authInfo.password} 
-					autoComplete="current-password"
-					onChange={handleChange}/>
-				<button type="submit" disabled={isLoading}>Submit</button>
-			</form>
-			{isRegister ?
-				<button onClick={() => setIsRegister(false)}>Already registered?</button>
-				:
-				<button onClick={() => setIsRegister(true)}>Register Instead</button>
-			}
+			<div className={styles.formDiv}>
+
+				{isRegister ?
+					<p>Sign up</p>
+					:
+					<p>Log in</p>
+				}
+				<form className={styles.form}
+					onSubmit={(event) => {
+						handleAuthSubmitClick(event, authInfo, setIsLoading, isRegister, setIsModal, auth);
+					}}>
+					<input 
+						placeholder="email" 
+						name="email" 
+						value={authInfo.email} 
+						autoComplete="username"
+						onChange={handleChange}/>
+					<input 
+						placeholder="password" 
+						name="password" 
+						type="password"
+						value={authInfo.password} 
+						autoComplete="current-password"
+						onChange={handleChange}/>
+					<button type="submit" disabled={isLoading}>Submit</button>
+				</form >
+				{isRegister ?
+					<button onClick={() => setIsRegister(false)}>Already registered?</button>
+					:
+					<button onClick={() => setIsRegister(true)}>Register Instead</button>
+				}
+			</div>
 		</main>
 	)
 
