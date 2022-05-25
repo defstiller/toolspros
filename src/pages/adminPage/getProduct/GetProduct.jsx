3156371a4734ec92d27e06fb9fa8da4a3ac16353
defstiller <81063003+defstiller@users.noreply.github.com>
2022-05-25 18:46:12 -0500

    added styles from parent to product card

--- src/pages/adminPage/getProduct/GetProduct.jsx
+++ src/pages/adminPage/getProduct/GetProduct.jsx
@@ -3,6 +3,7 @@ import React, {useEffect} from "react";
 import useAddGetData from "../../../logic/firebaseLogic/firebaseDB/useAddGetData";
 
 import ProductCard from "../../../components/productCard/productCardSmall/ProductCard";
+import styles from "./getProductStyles.module.css";
 
 function GetProduct() {
 	const {loading, error, receivedData, getData} = useAddGetData();
@@ -15,7 +16,7 @@ function GetProduct() {
 		<div>
 			{error && <p>error</p>}
 			{loading || receivedData.map(product => {
-				return <ProductCard product={product} key={product.name}/>;
+				return <ProductCard product={product} key={product.name} styles={styles}/>;
 			})}
 			{loading && <p>Loading...</p>}
 		</div>
