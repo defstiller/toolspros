61db6cff16c989ef049701116f5fb30af1ed2c0e
defstiller <81063003+defstiller@users.noreply.github.com>
2022-05-25 19:59:40 -0500

    Updates

--- src/context/context.js
+++
@@ -1,4 +0,0 @@
-import { createContext } from "react";
-export const ProductDataContext = createContext();
-export const AuthContext = createContext();
-export const ScreenResizeContext = createContext();
