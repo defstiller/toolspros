4502690fec499a3c1efa934e3915a537ffa79e3b
defstiller <81063003+defstiller@users.noreply.github.com>
2022-06-11 03:02:14 -0500

    complete modal rework

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,33 +1,60 @@
-import React, { useEffect} from "react";
+import React, { useEffect, useState} from "react";
 import PropTypes from "prop-types";
-function Modal(props) {
-	const {isModal, setIsModal, styles} = props;
-	useEffect(() => {
-		const timer = setTimeout(() => {
-			setIsModal({ // use as template for parent
-				isOpen: false,
-				content: "",
-				delay: 0
-			});
-		}, isModal.delay);
-		return () => clearTimeout(timer);
-	}, [isModal]);
 
-	if(isModal.isOpen) {
+import styles from "./modal.module.css";
+function Modal(props) {
+	const [isModal, setIsModal] = useState(false);
+	const {response, delay = 3000, customErrorDelay = 5000, error, setError, setResponse} = props;
+	/* open and close modal every time new response or error is received */
+	useEffect(() => {
+		if(response) {
+			setIsModal(true);
+			const timer = setTimeout(() => {
+				setIsModal(false);
+				setResponse(null);
+			}, delay);
+			return () => clearTimeout(timer);
+		}
+		if(error) {
+			setIsModal(true);
+			const timer = setTimeout(() => {
+				setIsModal(false);
+				setError(null);
+			}, delay + customErrorDelay);
+			return () => clearTimeout(timer);
+		}
+		return;
+	}, [response, error]);
+	function handleCloseModalClick() {
+		if(error) {setError(null);}
+		if(response) {setResponse(null);}
+		setIsModal(false);
+	}
+	if(isModal) {
 		return (
 			<main className={styles.modal}>
 				<div>
-					{isModal.content}
+					{response && response}
+					{error && 
+					<div>
+						<p>{error.message}</p>
+						<p>{error.code}</p>
+					</div>
+					}
 				</div>
+				<button onClick={handleCloseModalClick}>Close</button>
 			</main>
 		);
 	}
 }
 Modal.propTypes = {
 	props: PropTypes.any,
-	isModal: PropTypes.object, 
-	setIsModal: PropTypes.func,
-	styles: PropTypes.any
+	response: PropTypes.any,
+	delay: PropTypes.any,
+	error: PropTypes.any,
+	setError: PropTypes.any, 
+	setResponse: PropTypes.any,
+	customErrorDelay: PropTypes.any
 };
 
 export default Modal;
